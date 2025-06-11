5d19fc89309b29e8b1c71aa83780b428e51002af
Alex Sh <shalex.work@gmail.com>
2025-06-11 09:11:47 +0300

    Introduce centralized logging infrastructure using Spdlog

--- src/apps/main/main.cpp
+++ src/apps/main/main.cpp
@@ -1,58 +1,47 @@
 #include <iostream>
 #include <memory>
 
-#include "data/camera.hpp"
-#include "core/core.hpp"
-#include "api/controller.hpp"
-#include "common/logger.hpp"
-#include "common/config.hpp"
+#include "data/Camera.h"
+#include "core/Core.h"
+#include "api/Controller.h"
+#include "common/Logger/Logger.h"
+// #include "common/Logger/Config.h"
 
 int main() {
-    std::cout << APP_NAME << " v" << APP_VERSION_MAJOR << "." << APP_VERSION_MINOR << "." << APP_VERSION_PATCH << APP_VERSION_DIRTY << std::endl;
+    LOG_INFO("{} v{}.{}.{}{}", APP_NAME, APP_VERSION_MAJOR, APP_VERSION_MINOR, APP_VERSION_PATCH, APP_VERSION_DIRTY);
 
     //TODO: cross-cutting concerns (common)
-    auto config = std::make_shared<Config>();
-    try {
-        config->load_from_file("config/config.json");
-    } catch (const std::exception& e) {
-        std::cerr << "Error loading configuration: " << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    auto logger = std::make_shared<Logger>(config);
+    // auto config = std::make_shared<Config>();
+    // try {
+    //     config->load_from_file("config/config.json");
+    // } catch (const std::exception& e) {
+    //     std::cerr << "Error loading configuration: " << e.what() << std::endl;
+    //     return EXIT_FAILURE;
+    // }
 
     //TODO: create data access layer (camera)
-    logger->info("Initializing data access layer...");
-    auto camera = std::make_unique<Camera>(logger, config);
-    logger->info("Camera initialized");
+    LOG_INFO("Initializing data access layer...");
+    auto camera = std::make_unique<Camera>();
+    LOG_INFO("Camera initialized");
 
     //TODO: create business logic layer (core)
-    logger->info("Initializing business logic layer...");
-    auto core = std::make_unique<Core>(std::move(camera), logger, config);
-    logger->info("Core initialized");
+    LOG_INFO("Initializing business logic layer...");
+    auto core = std::make_unique<Core>(std::move(camera));
+    LOG_INFO("Core initialized");
 
     //TODO: create presentation layer (grpc)
-    logger->info("Initializing presentation layer...");
-    auto controller = std::make_unique<Controller>(std::move(core), logger, config);
-    logger->info("Controller initialized");
+    LOG_INFO("Initializing presentation layer...");
+    auto controller = std::make_unique<Controller>(std::move(core));
+    LOG_INFO("Controller initialized");
 
     try {
-        logger->info("Starting controller...");
-        controller->start();
-        logger->info("Controller started");
+        LOG_INFO("Starting controller...");
+        if (controller->start() != EXIT_SUCCESS) {
+            LOG_INFO("Controller started");
+            return EXIT_FAILURE;
+        }
     } catch (const std::exception& e) {
-        logger->error("Error during startup: {}", e.what());
-        return EXIT_FAILURE;
-    }
-
-    waitForTermination();
-
-    try {
-        logger->info("Stopping controller...");
-        controller->stop();
-        logger->info("Controller stopped");
-    } catch (const std::exception& e) {
-        logger->error("Error during shutdown: {}", e.what());
+        LOG_ERROR("Error during startup: {}", e.what());
         return EXIT_FAILURE;
     }
 
