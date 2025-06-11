746712000b9cf657651d4cffcc0e6887e0b4dc71
Alex Sh <shalex.work@gmail.com>
2025-06-11 17:13:58 +0300

    Split interfaces and concrete implementations

--- src/api/CameraController.h
+++ src/api/CameraController.h
@@ -3,31 +3,13 @@
 #include <string>
 #include <stdexcept>
 
+#include "api/ICameraController.h"
+
 namespace camera_service::core {
     class ICore;
 }
 
 namespace camera_service::api {
-    class ControllerException final : public std::runtime_error {
-    public:
-        explicit ControllerException(const std::string& message) : std::runtime_error(message) {
-        }
-    };
-
-    class ICameraController {
-    public:
-        virtual ~ICameraController() = default;
-
-        virtual bool start() = 0;
-        virtual void stop() = 0;
-
-        virtual bool setZoom(double zoomLevel) = 0;
-        virtual double getZoom() = 0;
-
-        virtual bool setFocus(double focusValue) = 0;
-        virtual double getFocus() = 0;
-    };
-
     class CameraController final : public ICameraController {
     public:
         explicit CameraController(std::unique_ptr<core::ICore> core);
---
+++ src/api/ICameraController.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <stdexcept>
+
+namespace camera_service::api {
+    class ControllerException final : public std::runtime_error {
+    public:
+        explicit ControllerException(const std::string& message) : std::runtime_error(message) {
+        }
+    };
+
+    class ICameraController {
+    public:
+        virtual ~ICameraController() = default;
+
+        virtual bool start() = 0;
+        virtual void stop() = 0;
+
+        virtual bool setZoom(double zoomLevel) = 0;
+        virtual double getZoom() = 0;
+
+        virtual bool setFocus(double focusValue) = 0;
+        virtual double getFocus() = 0;
+    };
+}
