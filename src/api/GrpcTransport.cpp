f056da3a829379da40bebddbfe3a09c0e93f6b31
Alex Sh <shalex.work@gmail.com>
2025-10-28 10:22:01 +0200

    Fix running multiple app instances on the same port

--- src/api/GrpcTransport.cpp
+++ src/api/GrpcTransport.cpp
@@ -1,5 +1,8 @@
 #include "GrpcTransport.h"
 
+#include <cstdlib>
+#include <fcntl.h>
+#include <unistd.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 
@@ -22,21 +25,35 @@ namespace camera_service::api {
     }
 
     Result<void> GrpcTransport::start(const std::string& server_address) {
+        LOG_DEBUG("Starting server...", server_address);
         //TODO: learn how to use health check
         grpc::EnableDefaultHealthCheckService(false);
         //TODO: disable in production
         grpc::reflection::InitProtoReflectionServerBuilderPlugin();
 
         grpc::ServerBuilder builder;
-        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+        builder.AddChannelArgument(GRPC_ARG_ALLOW_REUSEPORT, 0);
+        int selected_port = 0;
+        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials(), &selected_port);
         builder.RegisterService(callback_handler_.get());
 
+        // Temporarily suppress STDERR to hide gRPC internal error messages
+        const int stderr_backup = dup(STDERR_FILENO);
+        const int devnull = open("/dev/null", O_WRONLY);
+        dup2(devnull, STDERR_FILENO);
+        close(devnull);
+
         server_ = std::unique_ptr(builder.BuildAndStart());
-        if (!server_) {
-            return Result<void>::error("Failed to start the gRPC server");
+
+        // Restore STDERR
+        dup2(stderr_backup, STDERR_FILENO);
+        close(stderr_backup);
+
+        if (!server_ || selected_port == 0) {
+            return Result<void>::error("Failed to start server on: " + server_address);
         }
 
-        LOG_INFO("Listening on {}", server_address);
+        LOG_INFO("Server is listening on {}", server_address);
         return Result<void>::success();
     }
 
@@ -45,6 +62,7 @@ namespace camera_service::api {
             server_->Shutdown();
             server_.reset();
         }
+        LOG_DEBUG("Server stopped");
         return Result<void>::success();
     }
 
